651838058f0db8c202d20bf064c4a77e2426b89c
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-02-10 00:34:49 -0500

    Cleaned up code, revert part of code, new bugs found

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ class App extends Component {
     }
     if (e.currentTarget.value === '+' && this.state.displayValue !== '0') {
       this.getAns()
-        let value = this.state.displayValue
-        this.setState({
-          previousValue: value,
-          operation: 'addition',
-          waitingForNewValue: true,
-        })
+      let value = this.state.displayValue
+      this.setState({
+        previousValue: value,
+        operation: 'addition',
+        waitingForNewValue: true,
+      })
     }
     if (e.currentTarget.value === '-' && this.state.displayValue !== '0') {
       this.getAns()
@@ -80,13 +80,16 @@ class App extends Component {
     if (e.currentTarget.value === '=' && this.state.displayValue !== '0') {
       this.getAns();
     }
-  }
-  chkDot = () =>{
+    if (e.currentTarget.value === '.') {
       if (this.state.displayValue.indexOf('.') === -1) {
         this.setState({
-          displayValue: this.state.displayValue+'.',
+          displayValue: this.state.displayValue + '.',
         })
       }
+    }
+    // chkDot = () => {
+
+    //   }
   }
   getAns = () => {
     let value = parseFloat(this.state.previousValue);
@@ -114,17 +117,12 @@ class App extends Component {
   }
   //Get Number Event Listener for Number Buttons
   getNum = (e) => {
-    if ((this.state.displayValue === '0' || this.state.operation !== null )) {
+    if (this.state.displayValue === '0' || this.state.displayValue === this.state.previousValue || this.state.operation === 'equal') {
       this.setState({
         displayValue: e.currentTarget.value,
       })
-      if (this.state.operation === 'equal'){
-        this.setState({operation: null})
-      }
-      else if(this.state.operation !== null){
-        let value = parseFloat(this.state.displayValue);
-        this.setState({previousValue: value})
-      }
+      if (this.state.operation === 'equal')
+        this.setState({ operation: null })
     }
     else {
       let newValue = this.state.displayValue;
@@ -132,59 +130,44 @@ class App extends Component {
       this.setState({ displayValue: newValue })
     }
   }
-  //Waiting to display C or AC depending on Operator Buttons pressed.
-  isWaiting = () => {
-    let temp = this.state.waitingForNewValue;
-    if (temp) {
-      return (<TheButtons name="C" cb={this.operator} />)
-    }
-    else {
-      return (<TheButtons name="AC" cb={this.operator} />)
-    }
+  ButtonList = () => {
+    let Collection = ['AC', '%', '±', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=']
+    return Collection.map((e, i) => {
+      if (i === 0) {
+        let temp = this.state.waitingForNewValue;
+        if (temp) {
+          return (<TheButtons name="C" cb={this.operator} key={i} />)
+        }
+        else {
+          return (<TheButtons name="AC" cb={this.operator} key={i} />)
+        }
+      }
+      if (i === 3 || i === 7 || i === 11 || i === 15 || i === 18)
+        return <TheButtons name={e} orange="true" cb={this.operator} key={i} />
+      else if (i < 3) {
+        return <TheButtons name={e} cb={this.operator} key={i} />
+      }
+      else if (i === 17) {
+        return <TheButtons name={e} cb={this.operator} key={i} />
+      }
+      else if (i === 16) {
+        return <TheButtons name={e} big="true" cb={this.getNum} key={i} />
+      }
+      else {
+        return <TheButtons name={e} cb={this.getNum} key={i} />
+      }
+    })
   }
   //RENDER 
   render() {
     return (
       <div className="container" style={{ width: '50%' }}>
         <Screen displayValue={this.state.displayValue} onChange={this.getNum} />
-
         <div className="row">
-          {this.isWaiting()}
-          <TheButtons name="%" cb={this.operator} />
-          <TheButtons name="±" cb={this.operator} />
-          <TheButtons name="÷" orange="true" cb={this.operator} />
-        </div>
-
-        <div className="row">
-          <TheButtons name="7" cb={this.getNum} />
-          <TheButtons name="8" cb={this.getNum} />
-          <TheButtons name="9" cb={this.getNum} />
-          <TheButtons name="x" orange="true" cb={this.operator} />
-        </div>
-
-
-        <div className="row">
-          <TheButtons name="4" cb={this.getNum} />
-          <TheButtons name="5" cb={this.getNum} />
-          <TheButtons name="6" cb={this.getNum} />
-          <TheButtons name="-" orange="true" cb={this.operator} />
-        </div>
-
-        <div className="row">
-          <TheButtons name="1" cb={this.getNum} />
-          <TheButtons name="2" cb={this.getNum} />
-          <TheButtons name="3" cb={this.getNum} />
-          <TheButtons name="+" orange="true" cb={this.operator} />
-        </div>
-
-        <div className="row">
-          <TheButtons name="0" big="true" cb={this.getNum} />
-          <TheButtons name="." cb={this.chkDot} />
-          <TheButtons name="=" orange="true" cb={this.operator} />
+          {this.ButtonList()}
         </div>
       </div>
     );
-
   }
 }
 
